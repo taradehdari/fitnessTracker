1ee1174f1652561bebfb09f8692046a110923c40
Tara Dehdari <tara.dehdari@gmail.com>
2020-12-19 18:06:58 -0800

    took off quotations

--- models/workout.js
+++ models/workout.js
@@ -6,44 +6,44 @@ const workoutSchema = new Schema(
   {
     day: {
       type: Date,
-      default: () => new Date()
+      default: () => new Date(),
     },
     exercises: [
       {
         type: {
           type: String,
           trim: true,
-          required: "Enter an exercise type"
+          required: "Enter an exercise type",
         },
         name: {
           type: String,
           trim: true,
-          required: "Enter an exercise name"
+          required: "Enter an exercise name",
         },
         duration: {
           type: Number,
-          required: "Enter an exercise duration in minutes"
+          required: "Enter an exercise duration in minutes",
         },
         weight: {
-          type: Number
+          type: Number,
         },
         reps: {
-          type: Number
+          type: Number,
         },
         sets: {
-          type: Number
+          type: Number,
         },
         distance: {
-          type: Number
-        }
-      }
-    ]
+          type: Number,
+        },
+      },
+    ],
   },
   {
     toJSON: {
       // include any virtual properties when data is requested
-      virtuals: true
-    }
+      virtuals: true,
+    },
   }
 );
 
--- server.js
+++ server.js
@@ -15,9 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("process.env.MONGODB_URI,", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // routes
